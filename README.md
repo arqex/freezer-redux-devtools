47eb91aeae44baefeffc42e5f310ad05a1a407f0
marquex <javi@arqex.com>
2015-11-02 19:08:30 +0100

    Adds support for redux-devtools 3 and redux-devtools chrome extension.

--- README.md
+++ README.md
@@ -15,10 +15,15 @@ npm install --save freezer-redux-devtools
 ## Usage
 See the [freezer-redux-devtools example](https://github.com/arqex/freezer-redux-devtools-example) to see this module working.
 
+There are two ways of using this package:
+
+### Adding the devtools to your application
+This method needs your project to have redux, freezer-js and redux-devtools installed in order to work. freezer-redux-devtools won't install them for you.
+
 ```js
 import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import React, { Component } from 'react';
-import { getStore } from './freezer-redux-devtools/freezer-redux-devtools';
+import { getStore } from 'freezer-redux-devtools';
 import Freezer from 'freezer-js';
 
 // Our state is a freezer object
@@ -45,12 +50,42 @@ export default class App extends Component {
 }
 ```
 
+### Using redux-devtools chrome extension
+This way you don't need to install redux, or redux-devtools in your project. You need to have [redux-devtools chrome extension](https://github.com/zalmoxisus/redux-devtools-extension) instead.
+```js
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+import React, { Component } from 'react';
+import { supportChromeExtension } from 'freezer-redux-devtools/freezer-redux-middleware';
+import Freezer from 'freezer-js';
+
+// Our state is a freezer object
+var State = new Freezer({hello: 'world'});
+
+// Enable the extension
+supportChromeExtension( State );
+
+// You don't need anything special in your root component.
+export default class App extends Component {
+  render() {
+    return <MyApp state={ State.get() } />;
+    );
+  }
+  componentDidMount() {
+    State.on('update', () => this.forceUpdate());
+  }
+}
+```
+
+# How it works
 Every event triggered in `State` object will be shown in the `DevTools` sidebar. And all the actions dispatched by the `DevTools` will be reflected in 
 `State`-
 
 `freezer-redux-devtools` module exports:
 * `getStore( Freezer:State, boolean:persist=/[?&]debug_session=([^&]+)\b/)` creates a store to be used directly by `DevTools` component.
+
+`freezer-redux-devtools/freezer-redux-middleware` module exports:
 * `FreezerMiddleware( Freezer:State )` creates a redux store enhancer to let you create your own redux store.
+* `supportChromeExtension( Freezer:State )` turns on the redux-devtools chrome extension.
 
 ## Collaborate
 This project is in some way a fully functional proof of concept, but there is room for improvement:
--- freezer-redux-devtools.js
+++ freezer-redux-devtools.js
@@ -1,79 +1,6 @@
 var Redux = require('redux');
 var DevTools = require('redux-devtools');
-
-var ActionTypes = {
-	INIT: '@@INIT',
-	PERFORM_ACTION: 'PERFORM_ACTION'
-};
-
-/**
- * Redux middleware to make freezer and devtools
- * talk to each other.
- * @param {Freezer} State Freezer's app state.
- */
-function FreezerMiddleware( State ){
-	return function( next ){
-		return function FreezerStoreEnhancer( someReducer, someState ){
-			var commitedState = State.get(),
-				lastAction = 0,
-				/**
-				 * Freezer reducer will trigger events on any
-				 * devtool action to synchronize freezer's and
-				 * devtool's states.
-				 *
-				 * @param  {Object} state  Current devtool state.
-				 * @param  {Object} action Action being dispatched.
-				 * @return {Object}        Freezer state after the action.
-				 */
-				reducer = function( state, action ){
-					if( action.type == ActionTypes.INIT ){
-						State.set( state || commitedState );
-					}
-					else if( lastAction != ActionTypes.PERFORM_ACTION ) {
-						// Flag that we are dispatching to not
-						// to dispatch the same action twice
-						State.skipDispatch = 1;
-						State.trigger.apply( State, [ action.type ].concat( action.arguments || [] ) );
-					}
-					// The only valid state is freezer's one.
-					return State.get();
-				},
-				store = next( reducer ),
-				toolsDispatcher = store.devToolsStore.dispatch
-			;
-
-			// Override devTools store's dispatch, to set commitedState
-			// on Commit action.
-			store.devToolsStore.dispatch = function( action ){
-				var actionType = action.type,
-					states
-				;
-
-				lastAction = actionType;
-
-				toolsDispatcher.apply( store.devToolsStore, arguments );
-				return action;
-			};
-
-			// Dispatch any freezer "fluxy" event to let the devTools
-			// know about the update.
-			State.on('afterAll', function( reactionName ){
-				if( reactionName == 'update')
-					return;
-
-				// We don't dispatch if the flag is true
-				if( this.skipDispatch )
-					this.skipDispatch = 0;
-				else {
-					var args = [].slice.call( arguments, 1 );
-					store.dispatch({ type: reactionName, args: args });
-				}
-			});
-
-			return store;
-		};
-	};
-}
+var FreezerMiddleware = require('./freezer-redux-middleware');
 
 /**
  * Creates a redux store and adds FreezerMiddleware to it.
@@ -83,7 +10,7 @@ function FreezerMiddleware( State ){
  */
 function getStore( State, persist ){
 	var store = Redux.compose(
-		FreezerMiddleware( State ),
+		FreezerMiddleware.FreezerMiddleware( State ),
 		DevTools.devTools(),
 		DevTools.persistState( persist || window.location.href.match(/[?&]debug_session=([^&]+)\b/) )
 	)(Redux.createStore)( function( state ){ return state } );
@@ -92,6 +19,5 @@ function getStore( State, persist ){
 }
 
 module.exports = {
-	getStore: getStore,
-	FreezerMiddleware: FreezerMiddleware
+	getStore: getStore
 };
